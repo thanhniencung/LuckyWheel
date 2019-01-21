505833570bd935f592ab2a0a8133f079bb0aaa59
Betos, Peter Ian T. <peter.ian.t.betos@accenture.com>
2019-01-21 21:04:29 +0800

    Adjusting the app activity with the new parameters

--- app/src/main/java/com/ryan/luckywheel/MainActivity.java
+++ app/src/main/java/com/ryan/luckywheel/MainActivity.java
@@ -25,38 +25,38 @@ public class MainActivity extends Activity {
         final LuckyWheelView luckyWheelView = (LuckyWheelView) findViewById(R.id.luckyWheel);
 
         LuckyItem luckyItem1 = new LuckyItem();
-        luckyItem1.text = "100";
+        luckyItem1.topText = "100";
         luckyItem1.icon = R.drawable.test1;
         luckyItem1.color = 0xffFFF3E0;
         data.add(luckyItem1);
 
         LuckyItem luckyItem2 = new LuckyItem();
-        luckyItem2.text = "200";
+        luckyItem2.topText = "200";
         luckyItem2.icon = R.drawable.test2;
         luckyItem2.color = 0xffFFE0B2;
         data.add(luckyItem2);
 
         LuckyItem luckyItem3 = new LuckyItem();
-        luckyItem3.text = "300";
+        luckyItem3.topText = "300";
         luckyItem3.icon = R.drawable.test3;
         luckyItem3.color = 0xffFFCC80;
         data.add(luckyItem3);
 
         //////////////////
         LuckyItem luckyItem4 = new LuckyItem();
-        luckyItem4.text = "400";
+        luckyItem4.topText = "400";
         luckyItem4.icon = R.drawable.test4;
         luckyItem4.color = 0xffFFF3E0;
         data.add(luckyItem4);
 
         LuckyItem luckyItem5 = new LuckyItem();
-        luckyItem5.text = "500";
+        luckyItem5.topText = "500";
         luckyItem5.icon = R.drawable.test5;
         luckyItem5.color = 0xffFFE0B2;
         data.add(luckyItem5);
 
         LuckyItem luckyItem6 = new LuckyItem();
-        luckyItem6.text = "600";
+        luckyItem6.topText = "600";
         luckyItem6.icon = R.drawable.test6;
         luckyItem6.color = 0xffFFCC80;
         data.add(luckyItem6);
@@ -64,39 +64,39 @@ public class MainActivity extends Activity {
 
         //////////////////////
         LuckyItem luckyItem7 = new LuckyItem();
-        luckyItem7.text = "700";
+        luckyItem7.topText = "700";
         luckyItem7.icon = R.drawable.test7;
         luckyItem7.color = 0xffFFF3E0;
         data.add(luckyItem7);
 
         LuckyItem luckyItem8 = new LuckyItem();
-        luckyItem8.text = "800";
+        luckyItem8.topText = "800";
         luckyItem8.icon = R.drawable.test8;
         luckyItem8.color = 0xffFFE0B2;
         data.add(luckyItem8);
 
 
         LuckyItem luckyItem9 = new LuckyItem();
-        luckyItem9.text = "900";
+        luckyItem9.topText = "900";
         luckyItem9.icon = R.drawable.test9;
         luckyItem9.color = 0xffFFCC80;
         data.add(luckyItem9);
         ////////////////////////
 
         LuckyItem luckyItem10 = new LuckyItem();
-        luckyItem10.text = "1000";
+        luckyItem10.topText = "1000";
         luckyItem10.icon = R.drawable.test10;
         luckyItem10.color = 0xffFFE0B2;
         data.add(luckyItem10);
 
         LuckyItem luckyItem11 = new LuckyItem();
-        luckyItem11.text = "2000";
+        luckyItem11.topText = "2000";
         luckyItem11.icon = R.drawable.test10;
         luckyItem11.color = 0xffFFE0B2;
         data.add(luckyItem11);
 
         LuckyItem luckyItem12 = new LuckyItem();
-        luckyItem12.text = "3000";
+        luckyItem12.topText = "3000";
         luckyItem12.icon = R.drawable.test10;
         luckyItem12.color = 0xffFFE0B2;
         data.add(luckyItem12);
@@ -123,7 +123,7 @@ public class MainActivity extends Activity {
         luckyWheelView.setLuckyRoundItemSelectedListener(new LuckyWheelView.LuckyRoundItemSelectedListener() {
             @Override
             public void LuckyRoundItemSelected(int index) {
-                Toast.makeText(getApplicationContext(), data.get(index).text, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), data.get(index).topText, Toast.LENGTH_SHORT).show();
             }
         });
     }
